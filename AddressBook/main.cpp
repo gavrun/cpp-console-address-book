ad7b409e53c85b2dd88080f30dc34718de33e095
gavrun <38539655+gavrun@users.noreply.github.com>
2025-10-26 19:59:30 +0000

    Cleaned up

--- AddressBook/main.cpp
+++ AddressBook/main.cpp
@@ -10,44 +10,12 @@
 
 int main()
 {
-	std::cout << "Address Book - bootstrap = OK\n" << std::endl;
-
-	//Student s("Al", "Tor", "1234", "Dance");
-	//Teacher t("Ben", "Lock", "0974", "Math");
-	//Colleague c("Ann", "Lee", "5551", "Biology");
-
-	//s.printInfo();
-	//t.printInfo();
-	//c.printInfo();
-
-	//std::cout << std::endl;
-
-	std::cout << "Address Book - Domain storage + CRUD basic = OK\n" << std::endl;
-
-	//AddressBook bookTmp;
-
-	//bookTmp.addPerson(std::make_unique<Student>("Ban", "Cock", "0666", "Anatomy"));
-	//bookTmp.listPeople();
-	//bookTmp.remove(0);
-	//bookTmp.listPeople();
-
-	//std::cout << std::endl;
-
-	std::cout << "Address Book - Strategy search + Interface(UI) = OK\n" << std::endl;
-
-	//AddressBook addrBook;
-	//std::cout << "Welcome to Address Book application!\n"; // move to run()
-	//Interface ui(addrBook);
-	//ui.run();
-
-	std::cout << std::endl;
-
-	std::cout << "Address Book - Persistence by file + Fabric = OK\n" << std::endl;
-
 	AddressBook addrBook;
 	FileManager fm("contacts.txt");
 
 	/*
+	Test file already exists
+	
 	~\contacts.txt
 	Student | Al | Tor | 1234 | Dance
 	Teacher | Ben | Lock | 0974 | Math
